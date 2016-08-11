c498829458b5b3305c2a955fca161b4282d66862
WWWCourses <ivawebcourses@gmail.com>
2016-08-11 17:40:35 +0300

    HTML_Text_Formating_Elements Finished.Dark/Light themes added.

---
+++ outfit/js/slides.js
@@ -0,0 +1,4 @@
+function themeSwitcher(themeName){
+    alert("themeSwitcher ON");
+    document.getElementById('theme').setAttribute('href','/ProgressBG-Front-End-Dev/lib/reveal.js/css/theme/'+themeName+'.css');
+}
