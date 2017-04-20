994423b8ca808a461765fe039bdff3fc943e6dec
WWWCourses <ivawebcourses@gmail.com>
2017-04-20 11:30:58 +0300

    WIP: userWelcomeUpponAge

--- code/modules/getUserInput.js
+++ code/modules/getUserInput.js
@@ -14,7 +14,8 @@ var getUserInput = function(){
 	// actions
 	var id = function(nodeID){
 		var node  = document.getElementById(nodeID);
-		return node.value;
+		console.log(node);
+		return node ? node.value : null;
 	}
 
 	return {id: id};
