af367c64089a84ad4dff0bb3d93a1d52f08e85f1
WWWCourses <ivawebcourses@gmail.com>
2017-04-25 19:47:03 +0300

    HW&lab20 start

---
+++ labs/js/array_tic-tac-toe.js
@@ -0,0 +1,14 @@
+// var row1 = 	[ 1, 0, 1];
+// var row2 = 	[ undefined, undefined, undefined];
+// var row3 = 	[ undefined, undefined, undefined];
+
+var board = [
+	[ 0, 0, 'a'],
+	[ 0, 'b', 0],
+	[ 'c', 0, 0]
+];
+
+
+console.log( board[0][2] ); // a ?
+console.log( board[1][1] ); // b ?
+console.log( board[2][0] ); // c
