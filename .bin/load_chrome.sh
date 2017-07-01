361c1af81d9f22de834327dfe5f4819de79a8c87
WWWCourses <ivawebcourses@gmail.com>
2017-07-01 04:15:24 +0300

    structuring

---
+++ .bin/load_chrome.sh
@@ -0,0 +1,5 @@
+# Profile 1 = WD:ProgressBG
+# Profile 3 = phoneiep
+# Profile 4 = JSProgressBG
+# Profile 5 = JavelinJS
+google-chrome  --profile-directory="Profile 1" https://mail.google.com http://wwwcourses.github.io/ProgressBG-Front-End-Dev/
