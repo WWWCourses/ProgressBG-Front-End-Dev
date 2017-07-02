de6179782e1d956e804f7f4dda2414ef5ce07dbc
WWWCourses <ivawebcourses@gmail.com>
2017-07-02 13:44:55 +0300

    Lab18: 02.07.2017 DOne: arrays; TODO: HW

--- .bin/load_chrome.sh
+++ .bin/load_chrome.sh
@@ -2,4 +2,4 @@
 # Profile 3 = phoneiep
 # Profile 4 = JSProgressBG
 # Profile 5 = JavelinJS
-google-chrome  --profile-directory="Profile 1" https://mail.google.com http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/agenda/beginners.html
+google-chrome  --profile-directory="Profile 1" https://mail.google.com http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/agenda/beginners.html &
