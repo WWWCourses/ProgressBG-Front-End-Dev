49741536cc7070b8406abfa870bd52f6fe4b78c1
WWWCourses <ivawebcourses@gmail.com>
2017-10-04 19:22:17 +0300

    HTTP_Intro fixes

--- .bin/live-server.sh
+++ .bin/live-server.sh
@@ -1,3 +1,5 @@
-live-server --port=1111 --open=/ProgressBG-Front-End-Dev/pages/agenda/beginners.html \
+nohup live-server --port=1111 --open=/ProgressBG-Front-End-Dev/pages/agenda/beginners.html \
 --watch=/ProgressBG-Front-End-Dev   \
-/home/nemsys/projects/www/wwwcourses.github.io
+/home/nemsys/projects/www/wwwcourses.github.io &
+
+echo 'live-server started on port=1111 for ProgressBG-Front-End-Dev'
