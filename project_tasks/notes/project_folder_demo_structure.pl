3ca12564c8279e83bf9577684bec938804b9e5a7
WWWCourses <ivawebcourses@gmail.com>
2020-03-26 00:02:19 +0200

    course projets demo folder structure

---
+++ project_tasks/notes/project_folder_demo_structure.pl
@@ -0,0 +1,13 @@
+
+  mysite.com/
+  ├── images/
+  |       # the image files for your site
+  ├── pages
+  │   ├── about.html
+  │   └── contacts.html
+  |       # other html files for each page
+  ├──styles
+  |       # the css files for your site
+  └── index.html
+
+
