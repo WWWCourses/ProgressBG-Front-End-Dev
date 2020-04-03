dca8daccea1671051c23b5d38a63dfbe4d7934c5
WWWCourses <ivawebcourses@gmail.com>
2020-04-03 22:51:44 +0300

    update: todoApp vanilaJS

--- pages/themes/beginners/Appendix/todoApp_vanillaJS/main.js
+++ pages/themes/beginners/Appendix/todoApp_vanillaJS/main.js
@@ -1,71 +1,71 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // define variables and functions used
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const createTodoItem = function(todoText, parentNode) {
-    const li = document.createElement('li');
-    li.innerHTML = `
-        <span>${todoText}</span>
-        <i class="far fa-trash-alt removeTodo"></i>
-    `
-
-    parentNode.appendChild(li);
-};
-
-const countTodoItems = function(todoListUL) {
-    let count = todoListUL.children.length || 0;
-    return count;
-}
-
 const displayTodoItemsCount = function() {
-    let output = document.querySelector('.todoList .total>.output');
+    let count = nodes.todoListUL.children.length || 0;
 
-    output.innerHTML = countTodoItems(nodes.todoListUL);
+
+    nodes.totalItemsCount.innerHTML = count;
 }
-const addTodoHandler = function() {
+const addTodo = function() {
     // get the input text:
     const todoText = nodes.addTodoInput.value;
     console.log(todoText);
 
     // add todo item at the end
-    createTodoItem(todoText, nodes.todoListUL);
+    // createTodoItem(todoText, nodes.todoListUL);
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span>${todoText}</span>
+        <div class="removeTodo"><i class="far fa-trash-alt"></i></div>
+    `
+
+    nodes.todoListUL.appendChild(li);
 
     // clear input text
     nodes.addTodoInput.value = '';
 
     // focus on input for new todo:
     nodes.addTodoInput.focus();
+
+    // update todos count:
+    displayTodoItemsCount();
+}
+const removeTodo = function (e) {
+    console.log(e.target.classList);
+    console.log( e.target.parentNode.parentNode);
+    if(e.target.classList.contains('fa-trash-alt') ){
+        e.target.parentNode.parentNode.remove();
+
+        // update todos count:
+        displayTodoItemsCount();
+    }
 }
 
 let nodes = {
-    'todoListUL': document.querySelector('.todoList-items>ul'),
+    'todoListUL': document.querySelector('.todoListItems>ul'),
     'addTodoInput': document.querySelector('.addTodo>input'),
     'addTodoBtn': document.querySelector('.addTodo>.btnAdd'),
+    'totalItemsCount': document.querySelector('.todoApp .total>.output')
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // attach events
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-nodes.addTodoBtn.addEventListener('click', addTodoHandler);
-
-
+nodes.addTodoBtn.addEventListener('click', addTodo);
 
 nodes.addTodoBtn.addEventListener('keyup', function(e) {
     if(e.keyCode === 13){
-        addTodoHandler();
+        addTodo();
     }
 });
 nodes.addTodoInput.addEventListener('keyup', function(e) {
     if(e.keyCode === 13){
-        addTodoHandler();
+        addTodo();
     }
 })
 
 // to remove Todo Item:
-nodes.todoListUL.addEventListener('click', function(e) {
-    console.log(e.target);
-    if(e.target.classList.contains('removeTodo')){
-        e.target.parentNode.remove();
-    }
-})
+nodes.todoListUL.addEventListener('click', removeTodo, {capture: true})
 
 displayTodoItemsCount();
