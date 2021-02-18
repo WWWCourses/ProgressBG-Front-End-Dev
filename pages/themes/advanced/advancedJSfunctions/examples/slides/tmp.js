262a2037c6e7d369f91d4f6d7a5d5fd31da826ea
WWWCourses <ivawebcourses@gmail.com>
2021-02-18 18:16:31 +0200

    more on Recursion; Function best practices (to be continued); fixes

--- pages/themes/advanced/advancedJSfunctions/examples/slides/tmp.js
+++ pages/themes/advanced/advancedJSfunctions/examples/slides/tmp.js
@@ -1,17 +1,5 @@
-let x = 1, y=1; // x and y in global scope
-
-function foo(){
-  let x = 2, y=2;  // x and y in foo scope
-
-  function bar(){
-    let y = 3;  // y in bar scope
-    x = 3;     // uses x in foo scope
-    console.log(`x,y in bar: ${x}, ${y}`);
-  }
-
-  bar();
-  console.log(`x,y in foo: ${x}, ${y}`);
+function power(base, exponent) {
+    return exponent === 0 ? 1 : base * power(base, exponent - 1);
 }
 
-foo();
-console.log(`x,y in gloabl: ${x}, ${y}`);
+console.log(power(2, 10));
