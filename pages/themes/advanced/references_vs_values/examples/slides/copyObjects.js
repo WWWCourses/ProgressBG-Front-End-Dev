251daddbd01c5c2e48f9c858b2d708d81a3ecf4c
WWWCourses <ivawebcourses@gmail.com>
2021-02-25 18:58:45 +0200

    update

--- pages/themes/advanced/references_vs_values/examples/slides/copyObjects.js
+++ pages/themes/advanced/references_vs_values/examples/slides/copyObjects.js
@@ -2,14 +2,33 @@
 /*                        create an object shallow copy                       */
 /* -------------------------------------------------------------------------- */
 
-/* ----------------------- with Object.assign() method ---------------------- */
 // let user1 = {
 //     name: "Maria",
-//     age: 33
+//     age: 33,
+// 	colors:["red","green"]
 // }
+
+/* ----------------------- with Object.assign() method ---------------------- */
 // let user2 = Object.assign({}, user1);
+
+/* -------------------------- with spread operator -------------------------- */
+// let user2 = {...user1};
 
 // // we change a value in "user1", and that WILL NOT reflect the "user2" object:
 // user1.name = "Pesho";
 // console.log(`user1: ${user1.name}`);
 // console.log(`user2: ${user2.name}`);
+
+
+/* -------------------------------------------------------------------------- */
+/*                       create deep (clone) object copy                      */
+/* -------------------------------------------------------------------------- */
+let user1 = {
+    name: "Maria",
+    age: 33,
+	colors:["red","green"]
+}
+let user2 = JSON.parse(JSON.stringify(user1));
+
+user1.colors[0] = "black";
+console.log(user2.colors);
