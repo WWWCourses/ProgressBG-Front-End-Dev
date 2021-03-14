03f7adff2cc71e1ea65231294dbf125c38e6affc
WWWCourses <ivawebcourses@gmail.com>
2021-03-14 22:00:28 +0200

    updates && fixes

---
+++ pages/themes/advanced/regexes/examples/slides/passwordCheck.js
@@ -0,0 +1,18 @@
+// A valid password is:
+// 1. at least 6 symbols
+// 2. must contains letters [a-zA-Z], digits [\d] and symbols [!@#$%^&*]
+
+const passwords = [
+	'alabala', 	// false
+	'alaba#a', 	// false
+	'alaba9a', 	// false
+	'a1aba#9', 	// true
+	'a1@', 		// false
+];
+
+const re = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{6,}$/;
+
+passwords.forEach(str=>{
+	let matched = re.test(str);
+	console.log(`'${str}' => ${matched}`)
+});
